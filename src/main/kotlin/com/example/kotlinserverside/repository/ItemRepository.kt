f91e7a9936a02d9197968c12cdeff8dfc41c1527
勝田啓一 <katsutakeiichi@mba.local>
2020-11-17 18:36:27 +0900

    Improve Repository

--- src/main/kotlin/com/example/kotlinserverside/repository/ItemRepository.kt
+++ src/main/kotlin/com/example/kotlinserverside/repository/ItemRepository.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinserverside.repository
 
+import com.example.kotlinserverside.entity.Item
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
